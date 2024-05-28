6395b3887b12471bbfbc5e9d2b55101cce9909d9
Mohamed Darwesh <mohameddarwesh60@gmail.com>
2024-05-28 05:50:47 +0300

    fix: Improve responsive design for Login and SignUp components
     Adjusted padding and margins to ensure better responsiveness on smaller screens.

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 function SignUp() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
     <div className="flex min-h-screen flex-col items-center justify-center lg:p-24">
       <SignUpForm />
     </div>
